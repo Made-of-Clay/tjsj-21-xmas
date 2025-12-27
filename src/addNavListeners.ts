bf41cfc13a42c32cfb13015d3d59098daec0ef27
Adam Leis <adam.m.leis@gmail.com>
2025-12-27 11:05:44 -0500

    add more nav controls & a11y

--- src/addNavListeners.ts
+++ src/addNavListeners.ts
@@ -1,17 +1,29 @@
 function navToPrevious() {
-    const currentLink = document.querySelector<HTMLAnchorElement>(`[href="${location.hash}"]`);
-    if (!currentLink) {
-        return console.warn('Could not find a link for', location.hash);
+    let currentLink = document.querySelector<HTMLAnchorElement>(`[href="${location.hash}"]`);
+    
+    if (currentLink?.previousElementSibling instanceof HTMLAnchorElement) {
+        currentLink = currentLink.previousElementSibling;
+    } else {
+        currentLink = document.querySelector<HTMLAnchorElement>('nav a:first-of-type');
+        console.log('currentLink', currentLink);
     }
-    (currentLink.previousElementSibling as HTMLAnchorElement | null)?.click();
+    if (!currentLink) {
+        return console.warn('Could not find a link for', location.hash, ' or previous link');
+    }
+    currentLink.click();
 }
 
 function navToNext() {
-    const currentLink = document.querySelector<HTMLAnchorElement>(`[href="${location.hash}"]`);
-    if (!currentLink) {
-        return console.warn('Could not find a link for', location.hash);
+    let currentLink = document.querySelector<HTMLAnchorElement>(`[href="${location.hash}"]`);
+    if (currentLink?.nextElementSibling instanceof HTMLAnchorElement) {
+        currentLink = currentLink.nextElementSibling;
+    } else {
+        currentLink = document.querySelector<HTMLAnchorElement>('nav a:last-of-type');
     }
-    (currentLink.nextElementSibling as HTMLAnchorElement | null)?.click();
+    if (!currentLink) {
+        return console.warn('Could not find a link for', location.hash, ' or next link');
+    }
+    currentLink.click();
 }
 
 const prevKeys = ['ArrowLeft', 'ArrowUp', 'a', 'w'];
